f5740a112aef39bc2b697a05d7e89790397f9d5e
kdyst <k1671k@naver.com>
2023-08-09 11:17:31 +0900

    기본적인 Flask app 생성

---
+++ backend.py
@@ -0,0 +1,12 @@
+from flask import Flask #Flask app
+
+import bcrypt
+
+app = Flask(__name__)
+
+@app.route('/greet', methods=['GET'])
+def greeting():
+    return "Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
